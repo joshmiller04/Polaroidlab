13cbf4528dcb20bbc8daae0e160101e8b2d1f00f
joshmiller04 <mill3643@msu.edu>
2025-02-11 23:16:43 +0000

    css

--- .eleventy.js
+++ .eleventy.js
@@ -4,14 +4,3 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 };
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("Imgs"); // ✅ Copy images to _site
-
-  return {
-    dir: {
-      includes: "_includes",
-      input: ".",
-      output: "_site"
-    }
-  };
-};
