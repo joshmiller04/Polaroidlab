d5fd7b5fd407b76c9a3aea384eead5b3b03c0f0a
joshmiller04 <mill3643@msu.edu>
2025-02-12 16:51:00 +0000

    final

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,3 @@
-
-// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
